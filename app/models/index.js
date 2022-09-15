e79522d38eb6c6d999b569c6245d66c322f83d62
ChanhoPark2889 <102699225+ChanhoPark2889@users.noreply.github.com>
2022-09-14 21:18:33 -0500

    updated port and fixed model name to matched

--- app/models/index.js
+++ app/models/index.js
@@ -14,5 +14,5 @@ const sequelize = new Sequelize(dbConfig.DB, dbConfig.USER, dbConfig.PASSWORD, {
 const db = {};
 db.Sequelize = Sequelize;
 db.sequelize = sequelize;
-db.tutorials = require("./coursel.model.js")(sequelize, Sequelize);
+db.courses = require("./coursel.model.js")(sequelize, Sequelize);
 module.exports = db;
